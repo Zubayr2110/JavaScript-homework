00223e5061a6d019aff281856dc2ed7bdecbcd98
Zubayr2110 <webproject2110@gmail.com>
2023-08-13 21:43:55 +0500

    done

---
+++ main.js
@@ -0,0 +1,200 @@
+
+var a = 5, b = 4, c = 3, d = 7;
+
+
+// 4 - masala
+
+if (a > 0 && b > 0) console.log(true);
+else console.log(false);
+
+
+// 5-masala
+
+if (a > b && b > c) console.log('5-masala : true');
+else console.log('5-masala : false');
+
+// 5 - masala tugadi
+
+
+
+// 6-masala
+
+if (b > c && b < a) console.log('6-masala : true');
+else console.log('6-masala : false');
+
+// 6 - masala tugadi
+
+
+// 7-masala
+
+if (a % 2 != 0 && b % 2 != 0) console.log('7-masala : true');
+else console.log('7-masala : false');
+
+// 7 - masala tugadi
+
+
+// 8-masala
+
+if (a >= 0 && b >= 0 && c >= 0 && d >= 0) console.log('8-masala : true');
+else console.log('8-masala : false');
+
+// 8 - masala tugadi
+
+
+// 9-masala
+
+if (a >=0 && b >= 0 || a >= 0 && c >= 0 || b >= 0 && c >= 0) console.log('9-masala : true');
+else console.log('9-masala : false');
+
+// 9 - masala tugadi
+
+
+// 10-masala
+
+if (a == 1) console.log('Dushanba');
+if (a == 2) console.log('Seshanba');
+if (a == 3) console.log('Chorshanba');
+if (a == 4) console.log('Payshanba');
+if (a == 5) console.log('Juma');
+if (a == 6) console.log('Shanba');
+if (a == 7) console.log('Yakshanba');
+else console.log('Bunday hafta kuni mavjud emas');
+
+// 10 - masala tugadi
+
+
+// 11 - masala
+
+if (a == 90) console.log('Beeline');
+if (a == 99) console.log('UzMobile');
+if (a == 98) console.log('Perfectum');
+if (a == 93) console.log('Ucell');
+if (a == 71) console.log('Domashniy');
+if (a == 33) console.log('Humans');
+if (a == 97) console.log('Mobiuz');
+
+// 11 - masala tugadi
+
+
+
+// 12 - masala
+
+if (b > 0 ){
+    b ++;
+    console.log(b);
+}
+else{
+    b --;
+    console.log(b);
+}
+
+
+// 12 - masala tugadi
+
+
+// 13 - masala
+
+if (b > 0 ){
+    b += 3;
+    console.log(b);
+}
+else{
+    b -= 2;
+    console.log(b);
+}
+
+// 13 - masala tuagdi
+
+
+// 14 - masala
+
+if (a > b)console.log(a);
+else console.log(b);
+
+// 14 - masala tugadi
+
+
+// 15 - masala
+
+if (a > b && a > c) console.log(a);
+if (b > a && b > c) console.log(b);
+else console.log(c);
+
+// 15 - masala tugadi
+
+
+
+// 16 - masala
+
+if (a < b && a < c) console.log(a);
+if (b < a && b < c) console.log(b);
+else console.log(c);
+
+// 16 - masala tugadi
+
+
+// 17 - masala 
+
+
+
+// 17 - masala tugadi
+
+
+// 18 - masala
+
+if (a == 1) console.log('yomon');
+if (a == 2) console.log('qoniqarsiz');
+if (a == 3) console.log('qoniqarli');
+if (a == 4) console.log('yahshi');
+if (a == 5) console.log("a'lo");
+
+// 18 - masala tugadi
+
+
+
+// 19 - masala
+
+if (c % 2 == 0 && c > 0)console.log('just musbat');
+if (c % 2 != 0 && c > 0)console.log('toq musbat');
+if (c % 2 != 0 && c < 0)console.log('toq manfiy');
+if (c % 2 == 0 && c < 0)console.log('juft manfiy');
+
+// 19 - masala tugadi
+
+
+// 21 - masala
+
+if (a <= b <= c) console.log(true);
+else console.log(false);
+
+// 21 - masala tuagdi
+
+
+
+// 23 - masala 
+
+if (a > 0 && b > 0 && c > 0) console.log(true);
+else console.log(false);
+
+// 23 - masla tugadi
+
+
+// 24 - masala
+
+if (a > 0 && b < 0 && c < 0) console.log(a);
+if (b > 0 && a < 0 && c < 0) console.log(b);
+if (c > 0 && a < 0 && b < 0) console.log(c);
+else console.log(false);
+
+// 24 - masala tuagdi
+
+
+// 25 - masala
+
+var e = 493, a1, b1, c1;
+a1 = e % 10;
+b1 = e % 100 / 10;
+c1 = e / 100;
+if (a1 != b1 != c1)console.log(true);
+
+// 25 - masala tuagdi
